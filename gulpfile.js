601ac4bf3abde971b6c96412ba78d8f34301a73b
Daniel Rieck, Ph.D <danielcrieck@gmail.com>
2021-01-15 16:00:51 -0800

    Refactor gulp automation tasks
    Refactor implementation of gulp automation tasks in response to changes in dependency WSU-DAESA-JS/gulpCssJsBuilder.js.

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,48 @@
 /*!*************************************************************************************************
- * gulpfile.js
- * -------------------------------------------------------------------------------------------------
- * SUMMARY: Gulp-mediated automation of custom CSS and JS builds for WSU Office of Undergraduate
- * Education websites, including https://nsse.wsu.edu.
+ * █▀▀▀ █  █ █    █▀▀▄ █▀▀▀ ▀█▀ █    █▀▀▀      █ ▄▀▀▀
+ * █ ▀▄ █  █ █  ▄ █▄▄▀ █▀▀▀  █  █  ▄ █▀▀    ▄  █ ▀▀▀█
+ * ▀▀▀▀  ▀▀  ▀▀▀  █    ▀    ▀▀▀ ▀▀▀  ▀▀▀▀ ▀ ▀▄▄█ ▀▀▀ 
  *
- * AUTHOR: Daniel Rieck [daniel.rieck@wsu.edu] (https://github.com/invokeImmediately)
+ * Gulp-mediated automation of custom CSS and JS builds for the WSU NSSE website.
  *
- * REPOSITORY: https://github.com/invokeImmediately/nsse.wsu.edu
- *
- * LICENSE: ISC - Copyright (c) 2019 Daniel C. Rieck.
- *
- *   Permission to use, copy, modify, and/or distribute this software for any purpose with or
- *   without fee is hereby granted, provided that the above copyright notice and this permission
- *   notice appear in all copies.
- *
- *   THE SOFTWARE IS PROVIDED "AS IS" AND DANIEL RIECK DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS
- *   SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL
- *   DANIEL RIECK BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY
- *   DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF
- *   CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
- *   PERFORMANCE OF THIS SOFTWARE.
+ * @author Daniel Rieck [daniel.rieck@wsu.edu] (https://github.com/invokeImmediately)
+ * @link https://github.com/invokeImmediately/nsse.wsu.edu/blob/master/gulpfile.js
+ * @license MIT - Copyright (c) 2021 Washington State University
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ *     and associated documentation files (the “Software”), to deal in the Software without
+ *     restriction, including without limitation the rights to use, copy, modify, merge, publish,
+ *     distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
+ *     Software is furnished to do so, subject to the following conditions:
+ *   The above copyright notice and this permission notice shall be included in all copies or
+ *     substantial portions of the Software.
+ *   THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ *     BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ *     NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ *     DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *     FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  **************************************************************************************************/
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 // TABLE OF CONTENTS
 // -----------------
-//   §1: Variable Declarations................................................................41
-//     §1.1: Gulp task dependencies...........................................................44
-//   §2: Function declarations................................................................49
-//     §2.1: getCssBuildSettings()............................................................52
-//     §2.2: getJsBuildSettings().............................................................86
-//   §3: Main execution sequence.............................................................112
-//     §3.1: Set up gulp task for building CSS files.........................................115
-//     §3.2: Set up gulp task for building JS files..........................................120
+// §1: Gulp task dependencies..................................................................40
+// §2: Specificiation of build settings .......................................................45
+//   §2.1: getCssBuildSettings()...............................................................48
+//   §2.2: getJsBuildSettings()...............................................................130
+// §3: Entry point: Set up of build taks......................................................156
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
- 'use strict';
+( function() {
+
+'use strict';
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-// §1: Variable Declarations
+// §1: Gulp task dependencies
 
-////////
-// §1.1: Gulp task dependencies
-
-var gulpBuilder = require( './WSU-UE---JS/gulpBuilder.js' );
+var gulpBuilder = require( './WSU-DAESA-JS/gulpCssJsBuilder.js' );
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-// §2: Function declarations
+// §2: Specificiation of build settings 
 
 ////////
 // §2.1: getCssBuildSettings()
@@ -57,29 +53,77 @@ var gulpBuilder = require( './WSU-UE---JS/gulpBuilder.js' );
  * @return {object} - Instance of gulpBuilder.CssBuildSettings.
  */
 function getCssBuildSettings() {
-	var commentRemovalNeedle = /^(?:[ \t]*)?\/\*[^!].*$\n(?:^\*\*?[^/].*$\n)*\*\*?\/\n\n?/gm;
-	var dependenciesPath = './WSU-UE---CSS/';
-	var destFolder = './CSS/';
-	var fontImportStr = '@import url(\'https://fonts.googleapis.com/css?family=Roboto+Mono:400,700|\
-Roboto+Condensed:400,700|Roboto+Slab|PT+Serif\');\r\n';
-	var insertingMediaQuerySectionHeader = {
-			'before': /^@media/,
-			'lineBefore': '/*! ╔═══════════════════════════════════════════════════════════════════\
-════════════════════════════════════════════════════╗\r\n*   ║ MEDIA QUERIES ######################\
-################################################################################# ║\r\n*   ╚═══════\
-═══════════════════════════════════════════════════════════════════════════════════════════════════\
-═════════════╝\r\n*/',
-			'stopAfterFirstMatch': true
-		};
-	var minCssFileExtension = '.min.css';
-	var minCssFileHeaderStr = '/*! Built with the Less CSS preprocessor [http://lesscss.org/]. Plea\
+	minCssFileHeaderStr: '/*! Built with the Less CSS preprocessor [http://lesscss.org/]. Plea\
 se see [https://github.com/invokeImmediately/nsse.wsu.edu] for a repository of\
  source code. */\r\n';
  	var sourceFile = './CSS/nsse-custom.less';
 
-	return new gulpBuilder.CssBuildSettings(commentRemovalNeedle, dependenciesPath,
- 		destFolder, fontImportStr, insertingMediaQuerySectionHeader, minCssFileExtension,
- 		minCssFileHeaderStr, sourceFile);
+	return new gulpBuilder.CssBuildSettings( {
+		commentRemovalNeedle: /^(?:[ \t]*)?\/\*[^!].*$\n(?:^\*\*?[^/].*$\n)*\*\*?\/\n\n?/gm,
+		dependenciesPath = './WSU-UE---CSS/',
+		destFolder = './CSS/',
+		fontImportStr: '@import url(\'https://fonts.googleapis.com/css2?family=Open+Sans:ital,wgh' +
+			't@0,300;0,400;0,600;0,700;1,300;1,400;1,600;1,700&family=PT+Serif:ital,wght@0,400;0,' +
+			'700;1,400;1,700&family=Roboto+Condensed:ital,wght@0,400;0,700;1,400;1,700&family=Rob' +
+			'oto+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap\');\r\n',
+		insertingMediaQuerySectionHeader: {
+				before: /^@media/,
+				lineBefore: '/*! ========================================================================' +
+						'========================\r\n' +
+					'*** ▐▀▄▀▌█▀▀▀ █▀▀▄ ▀█▀ ▄▀▀▄   ▄▀▀▄ █  █ █▀▀▀ █▀▀▄ ▀█▀ █▀▀▀ ▄▀▀▀   ▄▀▀▀ █▀▀▀ ▄▀▀▀▐▀█▀▌' +
+						'▀█▀ ▄▀▀▄ ▐▀▀▄\r\n' +
+					'*** █ ▀ ▌█▀▀  █  █  █  █▄▄█   █  █ █  █ █▀▀  █▄▄▀  █  █▀▀  ▀▀▀█   ▀▀▀█ █▀▀  █     █  ' +
+						' █  █  █ █  ▐\r\n' +
+					'*** █   ▀▀▀▀▀ ▀▀▀  ▀▀▀ █  ▀    ▀█▄  ▀▀  ▀▀▀▀ ▀  ▀▄▀▀▀ ▀▀▀▀ ▀▀▀    ▀▀▀  ▀▀▀▀  ▀▀▀  █  ' +
+						'▀▀▀  ▀▀  ▀  ▐\r\n' +
+					'*** ==================================================================================' +
+						'==============\r\n' +
+					'*** Media queries built from precompiled CSS written in the Less language extension' +
+					  ' of CSS. Queries\r\n' +
+					'***   in this section are a combination of those designed for use on DAESA websites' +
+						' and those\r\n' +
+					'***   intended specifically for use on the NSSE website.\r\n' +
+					'***\r\n' +
+					'*** Fully documented, precompiled source code from which this section of stylesheet' +
+						' was developed\r\n' +
+					'***   is maintained on the following two GitHub projects:\r\n' +
+					'***   - https://github.com/invokeImmediately/WSU-UE---CSS/\r\n' +
+					'***   - https://github.com/invokeImmediately/nsse.wsu.edu/\r\n' +
+					'***\r\n' +
+					'*** @author Daniel Rieck [daniel.rieck@wsu.edu]' +
+						' (https://github.com/invokeImmediately)\r\n' +
+					'*** @license: MIT - Copyright (c) 2021 Washington State University\r\n' +
+					'***   Permission is hereby granted, free of charge, to any person obtaining a copy of' +
+						' this software\r\n' +
+					'***     and associated documentation files (the "Software"), to deal in the Software' +
+						' without\r\n' +
+					'***     restriction, including without limitation the rights to use, copy, modify,' +
+						' merge, publish,\r\n' +
+					'***     distribute, sublicense, and/or sell copies of the Software, and to permit' +
+						' persons to whom\r\n' +
+					'***     the Software is furnished to do so, subject to the following conditions:\r\n' +
+					'***   The above copyright notice and this permission notice shall be included in all' +
+						' copies or\r\n' +
+					'***     substantial portions of the Software.\r\n' +
+					'***   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR' +
+						' IMPLIED, INCLUDING\r\n' +
+					'***     BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR' +
+						' PURPOSE AND\r\n' +
+					'***     NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE' +
+						' FOR ANY\r\n' +
+					'***     CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR' +
+						' OTHERWISE,\r\n' +
+					'***     ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER' +
+						' DEALINGS IN THE\r\n' +
+					'***     SOFTWARE.\r\n' +
+					'*** ==================================================================================' +
+						'==============\r\n' +
+					'**/',
+				'stopAfterFirstMatch': true
+		},
+		minCssFileExtension: '.min.css',
+		minCssFileHeaderStr: ''
+	} );
 }
 
 ////////
@@ -93,11 +137,11 @@ se see [https://github.com/invokeImmediately/nsse.wsu.edu] for a repository of\
 function getJsBuildSettings() {
 	return {
 		buildDependenciesList: [
-			'./WSU-UE---JS/jQuery.oue-custom.js',
+			'./WSU-DAESA-JS/jQuery.daesa-custom.js',
 			'../qTip2/dist/jquery.qtip.min.js',
-			'./WSU-UE---JS/jQuery.qTip.js',
-			'./WSU-UE---JS/jQuery.cookieObjs.js',
-			'./WSU-UE---JS/jQuery.textResize.js',
+			'./WSU-DAESA-JS/jQuery.qTip.js',
+			'./WSU-DAESA-JS/jQuery.cookieObjs.js',
+			'./WSU-DAESA-JS/jQuery.textResize.js',
 			'./JS/nsse-custom.js'
 		],
 		commentNeedle: /^(\/\*)(?!!)/g,
@@ -109,14 +153,9 @@ function getJsBuildSettings() {
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-// §3: Main execution sequence
-
-////////
-// §3.1: Set up gulp task for building CSS files
+// §3: Entry point: Set up of build taks
 
 gulpBuilder.setUpCssBuildTask( getCssBuildSettings() );
-
-////////
-// §3.2: Set up gulp task for building JS files
-
 gulpBuilder.setUpJsBuildTask( getJsBuildSettings() );
+
+} )();
